6555195a38092a5a5567bd0b749a3b418ac1d0d6
Jason Llanes <llanezjaizon@gmail.com>
2026-09-21 09:24:14 +0800

    Updates

---
+++ assets/map-pin-kGQUj6qO.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-CwvCECS2.js";const e=a("Folder",[["path",{d:"M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z",key:"1fr9dc"}]]),r=a("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);export{e as F,r as M};
